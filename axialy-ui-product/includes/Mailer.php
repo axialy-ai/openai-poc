9fd3f1908f0f40784463de8de2cf91c9b7caa507
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-18 20:52:19 -0700

    Update Mailer.php

--- axialy-ui-product/includes/Mailer.php
+++ axialy-ui-product/includes/Mailer.php
@@ -14,6 +14,11 @@
 
 namespace AxiaBA;
 
+// Ensure Composer autoloader is loaded
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
